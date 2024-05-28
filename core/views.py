9eb640e4b739677c0d0496902f6a269fa4994955
esteban93040 <estebanzespinosa1994@gmail.com>
2024-05-28 14:38:54 -0500

    registro de usuarios listo

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import Group
+from django.contrib.auth import authenticate, login
+from .forms import *
 
 
 #@login required se utiliza para validar que el usuario este logueado para poder acceder a la pagina respectiva
@@ -17,17 +19,26 @@ def home(request):
 @login_required
 def homeCapataz(request): #Pagina de inicio de los Capataz
     if request.user.groups.filter(name='Capataz').exists():#Se realiza otra validacion al momento de ingresar a la pagina
-        return render(request,'core/homeCapataz.html')
+        return render(request,'frontend/homeCapataz.html')
 
 @login_required
 def homeGerente(request): #Pagina de inicio del gerente
-    if request.user.is_superuser: #Se realiza otra validacion al momento de ingresar a la pagina
-        return render(request,'core/homeGerente.html')
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                group = form.cleaned_data['group']
+                group.user_set.add(user)
+                return redirect('homeGerente')
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'frontend/homeGerente.html', {'form': form})
 
 @login_required
 def homeDirector(request): #Pagina de inicio del director
     if request.user.groups.filter(name='Director').exists():#Se realiza otra validacion al momento de ingresar a la pagina
-     return render(request,'core/homeDirector.html')
+     return render(request,'frontend/homeDirector.html')
     
 def exit(request): # el exit define que el usuario cerro sesión y redirige al home donde al no estar logueado se va directamente al login
     logout(request)
@@ -36,7 +47,7 @@ def exit(request): # el exit define que el usuario cerro sesión y redirige al h
 def group_users(request): #Esta es una prueba para listar usuarios pertenecientes a un grupo, aun no esta lista
     group = get_object_or_404(Group, name='Capataz')
     users = group.user_set.all()
-    return render(request, 'core/group_users.html', {'group': group, 'users': users})
+    return render(request, 'frontend/group_users.html', {'group': group, 'users': users})
 
 
 
